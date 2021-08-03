0ad7636379424d920aa6535f6c47158363943f63
furrutiav <74210202+furrutiav@users.noreply.github.com>
2021-08-03 12:48:22 -0400

    initial commint

---
+++ src/js/postbp_.js
@@ -0,0 +1,49 @@
+
+function postImage() {
+    var bp = document.getElementById("bp")
+    var apiurl = "http://felipe.demon.live:5000"
+    var bp64= getBase64Image(bp)
+
+    post(bp64, apiurl)
+}
+
+function getBase64Image(img) {
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+    var dataURL = canvas.toDataURL("image/png");
+    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+  }
+
+  function post(bp64, apiurl) {
+    $.ajax
+    ({
+        type: "POST",
+        url: apiurl,
+        dataType: 'json',
+        contentType: 'application/json',
+        async: false,
+        data: JSON.stringify({ "test": bp64 }),
+        success: function (data) {
+            let perceptron = data.TEST.models.perceptron
+            let svm = data.TEST.models.svm
+            let nb = data.TEST.models.NB
+            let dummy = data.TEST.models.dummy
+            let dt = data.TEST.models["Decision-Tree"]
+            let solution = data.TEST.solution
+            let scores = "Perceptron: " + perceptron + "<br>" + "SVM: " + svm + "<br>" + "NB: " + nb + "<br>" + "Dummy: " + dummy + "<br>" + "Decision Tree: " + dt;
+            let atributos = "Atributos: "
+            for (let i in solution){
+                atributos = atributos + solution[i] + " ";
+            }
+            let array1 = [perceptron,svm,nb,dummy,dt]
+            let max = Math.max(...array1)
+            $('#scores').html(scores)
+            $('#atributos').html(atributos)
+            $('#score').html(max)
+
+        }
+    })
+}
